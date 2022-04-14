0dd3103195074568f82de0e1c2c3a16e5f080baa
AneekBiswas <56aneek54k@gmail.com>
2022-04-14 21:36:04 +0530

    Update Cricket.py

--- Games/INDEV/Cricket.py
+++ Games/INDEV/Cricket.py
@@ -2,8 +2,7 @@
 import random
 
 #Introduction an Instruction
-print(''' ~~~~~~~~~~ Game of Cricket ~~~~~~~~~~\nInstructions:\n\t1. You have to select any random number from 1 to 6.\n\t2. The computer will also select a number.\n\t3. While batting, if the number selected by you and computer is different, then your number will add to your runs.\n\tIf the number selected by you and computer is same, then you will lose your wicket.\n\t4. While bowling,if the number selected by you and computer is different,then the computer's number will add to its runs.\n\tIf the number selected by you and computer is same, then the computer will lose its wicket.\n\t5. Each player will get 10 wickets and overs depend on the type you chose for batting and bowling.\n\t\ta.Super Over: You get 1 over (6 Balls) and 3 wickets\n\t\tb.100 Ball Cricket: You get 100 balls (16 overs 4 balls)\n\t\tc.T20: You get 20 overs (120 balls)\n\t\td.ODI: You get 50 overs (300 balls)\n\t\te.Test Match: You get 90 overs(540 balls)\n\t6. The inning will end after either the three wickets fell or the overs end.\n\t7. The player with maximum runs wins.\n\t8. Press Enter/Return to start. ''')
-input()
+input(''' ~~~~~~~~~~ Game of Cricket ~~~~~~~~~~\nInstructions:\n\t1. You have to select any random number from 1 to 6.\n\t2. The computer will also select a number.\n\t3. While batting, if the number selected by you and computer is different, then your number will add to your runs.\n\tIf the number selected by you and computer is same, then you will lose your wicket.\n\t4. While bowling,if the number selected by you and computer is different,then the computer's number will add to its runs.\n\tIf the number selected by you and computer is same, then the computer will lose its wicket.\n\t5. Each player will get 10 wickets and overs depend on the type you chose for batting and bowling.\n\t\ta.Super Over: You get 1 over (6 Balls) and 3 wickets\n\t\tb.100 Ball Cricket: You get 100 balls (16 overs 4 balls)\n\t\tc.T20: You get 20 overs (120 balls)\n\t\td.ODI: You get 50 overs (300 balls)\n\t\te.Test Match: You get 90 overs(540 balls)\n\t6. The inning will end after either the three wickets fell or the overs end.\n\t7. The player with maximum runs wins.\n\t8. Press Enter/Return to start. ''')
 
 def ball(game):# to get the number of balls
     if game == 'a': balls = 6
@@ -45,6 +44,7 @@ elif random_toss != toss:
     if random_opt == 1:
         com_opt = 'bat'
     else: com_opt = 'ball'
+
 if u_result == 'w':
     if user_opt == 'bat':
         Bat_first = 'You'
@@ -60,8 +60,7 @@ else:
         Ball_first = 'Computer'
         Bat_first = 'You'
 
-print('Press Enter/Return for First Inning')
-input() 
+input('Press Enter/Return for First Inning')
 
 # First Inning
 while True:
@@ -83,14 +82,11 @@ while True:
             print('\nScore =',runs_1,'/',wickets_1,'\nBalls remaining: ', balls_1)
             balls_1 -= 1
             if balls_1%6 == 0 and game != 'b':
-                print('End of Over. Press Enter/Return to continue')
-                input()
+                input('End of Over. Press Enter/Return to continue')
             elif balls_1%6 == 4 and game == 'b':
-                print('End of Over. Press Enter/Return to continue')
-                input()
+                input('End of Over. Press Enter/Return to continue')
     print('\n---------- End of Inning ----------')
-    print('\nFinal Score: \nRuns =',runs_1, '\nwickets =',iwicket-wickets_1,'\n',Ball_first,'needs',runs_1 + 1,'runs to win.\n','Press Enter/Return to begin Second Inning')
-    input()
+    input('\nFinal Score: \nRuns =',runs_1, '\nwickets =',iwicket-wickets_1,'\n',Ball_first,'needs',runs_1 + 1,'runs to win.\n','Press Enter/Return to begin Second Inning')
     # Second Inning 
     runs_2 = 0# to count the running runs in second inning
     wickets_2 = wicket(game)# to count the running wickets in second inning
@@ -111,11 +107,9 @@ while True:
             else: print('\nScore =',runs_2,'/',wickets_2,)
             balls_2 -= 1
             if balls_2%6 == 0 and game != 'b':
-                print('End of Over. Press Enter/Return to continue')
-                input()
+                input('End of Over. Press Enter/Return to continue')
             elif balls_2%6==4 and game == 'b':
-                print('End of Over. Press Enter/Return to continue')
-                input()
+                input('End of Over. Press Enter/Return to continue')
             if game == 'e':
                 #Third Inning begins
                 print('\n---------- Third Inning Begins ----------')
@@ -133,10 +127,29 @@ while True:
                             else: runs_1 += c_choice
                         balls_3 -= 1
                         if balls_3%6 == 0:
-                            print('End of Over. Press Enter/Return to Continue')
-                            input()
-                    
-    print('\n---------- End of Inning ----------','\nFinal Score:\n','Runs =',runs_2,'\nwickets =',wickets_2,'\nPress Enter/Return to view results'), input() 
+                            input('End of Over. Press Enter/Return to Continue')
+                    if runs_2 > runs_1:
+                        input('\n---------- End of Inning ----------','\nFinal Score',runs_1, '\nwickets =',iwicket-wickets_1)
+                        break
+                    else: input('\n---------- End of Inning ----------','\nFinal Score',runs_1, '\nwickets =',iwicket-wickets_1,'\n',Ball_first,'needs',(runs_1 + 1 - runs_2),'runs to win.\n','Press Enter/Return to begin Fourth Inning')
+                print('\n---------- Fourth Inning Begins ----------')
+                wickets_4 = wicket(game)
+                balls_4 = ball(game)
+                while wickets_4 != 0 or balls_4 != 0 or runs_2 < runs_1 + 1:
+                    u_choice = int(input('\n Choose any number from 1 to 6'))
+                    c_choice = random.randint(1,6)
+                    if u_choice < 1 or u_choice > 6 or type(u_choice) == "<class 'str'>": print('\nPlease choose a value from 1 to 6.')
+                    else:
+                        print('Your Choice: ', u_choice, '\nComputer\'s Choice: ', c_choice)
+                        if u_choice == c_choice: wickets_4 -= 1
+                        else:
+                            if Ball_first == 'You': runs_2 += u_choice
+                            else: runs_2 += c_choice
+                        balls_4 -= 1
+                        if balls_4%6==0: input('End of Over. Press Enter/Return to Continue')
+
+
+    input('\n---------- End of Inning ----------','\nFinal Score:\n','Runs =',runs_2,'\nwickets =',wickets_2,'\nPress Enter/Return to view results')
     # Result of Match 
     print('\n~~~~~~~~~~ Result ~~~~~~~~~~')
     if runs_1 > runs_2:    
@@ -144,8 +157,7 @@ while True:
         else: print('\nBetter luck next time! The Computer won the Match by',runs_1 - runs_2,'runs.')
     elif runs_2 > runs_1:
         if Ball_first == 'You': print('\nCongratulations! You won the Match by',iwicket - wickets_2,'wickets.')
-        else: print('\nBetter luck next time! The Computer won the Match by',iwicket - wickets_2,'wickets.')
-            
+        else: print('\nBetter luck next time! The Computer won the Match by',iwicket - wickets_2,'wickets.')        
     else:
         tiebreaker = input('The Match is a Tie.','\nNo one Wins.\nWant a tie breaker?(y/n): ')
         if tiebreaker == 'y':
